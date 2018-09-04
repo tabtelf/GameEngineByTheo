5a61789c74064c25e0fdabd9a1b121f45f903ac8
tabtelf <tabtelf@icloud.com>
2018-09-04 11:32:27 +0800

    i

---
+++ Framework/Common/BaseApplication.cpp
@@ -0,0 +1,20 @@
+#include "BaseApplication.hpp"
+
+// Parse command line, read configuration, initialize all sub modules
+int My::BaseApplication::Initialize(){
+
+  m_bQuit = false;
+
+  return 0;
+}
+
+// Finalize all sub modules and clean up all runtime temporary files.
+void My::BaseApplication::Finalize(){}
+
+// one cycle of the main loop
+void My::BaseApplication::Tick(){}
+
+bool My::BaseApplication::IsQuit(){
+
+  return m_bQuit;
+}
